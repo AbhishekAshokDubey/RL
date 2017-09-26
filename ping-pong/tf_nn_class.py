d0de9f979163efa6b2b24c822364c798f02fccba
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2017-09-26 16:15:15 +0530

    Created tf_nn_class.py

--- ping-pong/tf_nn_class.py
+++ ping-pong/tf_nn_class.py
@@ -18,12 +18,12 @@ class neural_network():
         self.w_variables = tf.trainable_variables()
         self.opt = tf.train.RMSPropOptimizer(decay = decay_rate, learning_rate=learning_rate)
 		
-	self.train = self.opt.minimize(self.policy_loss)
+        self.train = self.opt.minimize(self.policy_loss)
 		
-	# below code for accumulating grads and applying after n steps
+        # below code for accumulating grads and applying after n steps
         self.accum_grad_variables = [tf.Variable(tf.zeros_like(w), trainable=False) for w in self.w_variables]
         self.grads_and_vars = self.opt.compute_gradients(self.policy_loss, self.w_variables)
         self.accum_grad_ops = [self.accum_grad_variables[i].assign_add(grad_and_var[0]) for i, grad_and_var in enumerate(self.grads_and_vars)]
         self.train_step = self.opt.apply_gradients([(self.accum_grad_variables[i], grad_and_var[1]) for i, grad_and_var in enumerate(self.grads_and_vars)])
-		self.reset_accum_grads_variables_ops = [w.assign(tf.zeros_like(w)) for w in self.accum_grad_variables]
+        self.reset_accum_grads_variables_ops = [w.assign(tf.zeros_like(w)) for w in self.accum_grad_variables]
         self.saver = tf.train.Saver()
